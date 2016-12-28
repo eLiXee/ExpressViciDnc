2b9135a6839d7a4b45c5b7b20370328c0304c54c
arronnandotech <arronbq@gmail.com>
2016-12-27 19:44:22 -0500

    clean up useless code

--- src/routes/dncRoutes.js
+++ src/routes/dncRoutes.js
@@ -2,37 +2,38 @@ var express = require('express');
 var dncRouter = express.Router();
 var mySql = require('mysql');
 
-var viciConnection = mySql.createPool({
+dncRouter.get('/dnc', function (req, res) {
+    var phoneNumber = req.query.phoneNumber;
+    var viciConnection = mySql.createPool({
     connectionLimit: 100,
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'asterisk',
     debug: false
-});
-
-dncRouter.get('/dnc', function (req, res) {
-    var phoneNumber = req.query.phoneNumber;
+    });
 
     viciConnection.getConnection(function (err, connection) {
         if (err) {
             connection.release();
-            res.json({ "code": 100, "status": "Error connecting to database" });
-
-        }
-
+            res.json({ 
+                "code": 100, "status": "Error connecting to database" 
+            });
+        };
         var query = "insert into dnc_list VALUES (" + phoneNumber + ')';
         connection.query(query, function (err, rows) {
             connection.release();
             if (!err) {
-                res.json({ "Inserted": "true", "phoneNumber": phoneNumber });
-            }
+                res.json({ 
+                    "Inserted": "true", "phoneNumber": phoneNumber 
+                });
+            };
         });
         connection.on('error', function (err) {
             connection.release();
-            res.json({ "code": 100, "status": "Error Connecting to database" });
-
-
+            res.json({ 
+                "code": 100, "status": "Error Connecting to database" 
+            });
         });
     });
 });
