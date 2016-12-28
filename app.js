256ff4294744af2262970fa9e6810419f96c59a2
arronnandotech <arronbq@gmail.com>
2016-12-28 16:17:38 -0500

    added delete route / fix else for if number exists

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-var port = 80;
+var port = 5000;
 
 var dncRouter = require('./src/routes/dncRoutes');
 app.get('/', function (req, res) {
     res.send('Nothing here')
 });
 
-app.get('/dnc', dncRouter);
+app.use('/api', dncRouter);
 
 app.listen(port, function (err) {
     console.log('Vici Api running on port ' + port);
--- src/routes/dncRoutes.js
+++ src/routes/dncRoutes.js
@@ -2,40 +2,85 @@ var express = require('express');
 var dncRouter = express.Router();
 var mySql = require('mysql');
 
-dncRouter.get('/dnc', function (req, res) {
-    var phoneNumber = req.query.phoneNumber;
-    var viciConnection = mySql.createPool({
+var viciConnection = mySql.createPool({
     connectionLimit: 100,
     host: 'localhost',
-    user: 'root',
+    user: 'cron',
     password: '',
     database: 'asterisk',
     debug: false
     });
 
+dncRouter.get('/', function(req,res){
+    res.send("Nothing Here");
+})
+dncRouter.post('/dnc', function (req, res) {
+    var phoneNumber = req.query.phone_number;
     viciConnection.getConnection(function (err, connection) {
-        if (err) {
-            connection.release();
+        if (err) {           
             res.json({ 
                 "code": 100, "status": "Error connecting to database" 
             });
         };
-        var query = "insert into dnc_list VALUES (" + phoneNumber + ')';
-        connection.query(query, function (err, rows) {
-            connection.release();
-            if (!err) {
+        var query = "insert into vicidial_dnc VALUES (" + phoneNumber + ')';
+        connection.query(query, function (err, rows,fields) {
+             if (!err) {
                 res.json({ 
-                    "Inserted": "true", "phoneNumber": phoneNumber 
+                    "Inserted": "true", "phoneNumber": phoneNumber
                 });
-            };
+            }
+            else{
+                res.json({"Inserted":"false", "phoneNumber":phoneNumber,"reason":err});
+            }
         });
         connection.on('error', function (err) {
-            connection.release();
+           
             res.json({ 
                 "code": 100, "status": "Error Connecting to database" 
             });
+           
         });
-    });
+        connection.release();
+    }); 
+})
+
+    .delete('/dnc', function(req,res){
+    var phoneNumber = req.query.phone_number;
+    viciConnection.getConnection(function (err, connection){
+      if (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error connecting to database" 
+            });
+        };
+        var query = "DELETE from vicidial_dnc where phone_number = '" + phoneNumber + "'";
+        connection.query(query, function (err, rows) {
+           
+             if(rows.affectedRows == 0)
+            {
+                res.send(phoneNumber + " is not currently in the DNC")
+                return;
+            }
+            if (!err) {
+                res.json({ 
+                    "Deleted": "true", "phoneNumber": phoneNumber 
+                });
+                return;
+            }           
+            else{
+                res.send(err);
+            }
+        });
+        connection.on('error', function (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error Connecting to database" 
+            });
+
+        });
+        connection.release();
+    });        
 });
+
 
 module.exports = dncRouter;     
