733d09894ce6afcb93cc7a8260e96f6e7fe31682
arronnandotech <arronbq@gmail.com>
2016-12-28 17:58:19 -0500

    added phoneNUmber length check and apikey

--- src/routes/dncRoutes.js
+++ src/routes/dncRoutes.js
@@ -5,24 +5,73 @@ var mySql = require('mysql');
 var viciConnection = mySql.createPool({
     connectionLimit: 100,
     host: 'localhost',
-    user: 'cron',
+    user: 'root',
     password: '',
     database: 'asterisk',
     debug: false
     });
+var apiPass = '';
 
-dncRouter.get('/', function(req,res){
-    res.send("Nothing Here");
-})
-dncRouter.post('/dnc', function (req, res) {
+dncRouter.get('/dnc', function(req,res){
     var phoneNumber = req.query.phone_number;
+    var apiKey = req.query.api_key;
+    if (apiKey != apiPass)
+    {
+        res.json({"Inserted":"false", "reason":"Invalid API Key"})
+        return;
+    }
+    if ((phoneNumber.length > 11 || phoneNumber.length < 10))
+    {
+        res.json({"Inserted":"false", "reason":"Phone Number not 10 or 11 Digits"})
+        return;
+    }
+    viciConnection.getConnection(function (err, connection) {
+        if (err) {           
+            res.json({ 
+                "code": 100, "status": "Error connecting to database"})
+                
+        };
+        var query = "INSERT INTO vicidial_dnc VALUES (" + phoneNumber + ')';
+        connection.query(query, function (err, rows,fields) {
+             if (!err) {
+                res.json({ 
+                    "Inserted": "true", "phoneNumber": phoneNumber
+                });
+            }
+            else{
+                res.json({"Inserted":"false", "phoneNumber":phoneNumber,"reason":err});
+            }
+        });
+        connection.on('error', function (err) {
+           
+            res.json({ 
+                "code": 100, "status": "Error Connecting to database" 
+            });
+           
+        });
+        connection.release();
+    }); 
+})
+
+    .post('/dnc', function (req, res) {
+    var phoneNumber = req.query.phone_number;
+     if (apiKey != apiPass)
+    {
+        res.json({"Inserted":"false", "reason":"Invalid API Key"})
+        return;
+    }
+    if ((phoneNumber.length > 11 || phoneNumber.length < 10))
+    {
+        res.json({"Inserted":"false", "reason":"Phone Number not 10 or 11 Digits"})
+        return;
+    }
     viciConnection.getConnection(function (err, connection) {
         if (err) {           
             res.json({ 
                 "code": 100, "status": "Error connecting to database" 
             });
         };
-        var query = "insert into vicidial_dnc VALUES (" + phoneNumber + ')';
+        var query = "INSERT INTO vicidial_dnc VALUES (" + phoneNumber + ')';
         connection.query(query, function (err, rows,fields) {
              if (!err) {
                 res.json({ 
@@ -46,6 +95,16 @@ dncRouter.post('/dnc', function (req, res) {
 
     .delete('/dnc', function(req,res){
     var phoneNumber = req.query.phone_number;
+     if (apiKey != apiPass)
+    {
+        res.json({"Inserted":"false", "reason":"Invalid API Key"})
+        return;
+    }
+    if ((phoneNumber.length > 11 || phoneNumber.length < 10))
+    {
+        res.json({"Inserted":"false", "reason":"Phone Number not 10 or 11 Digits"})
+        return;
+    }
     viciConnection.getConnection(function (err, connection){
       if (err) {
             
@@ -53,7 +112,7 @@ dncRouter.post('/dnc', function (req, res) {
                 "code": 100, "status": "Error connecting to database" 
             });
         };
-        var query = "DELETE from vicidial_dnc where phone_number = '" + phoneNumber + "'";
+        var query = "DELETE FROM vicidial_dnc where phone_number = '" + phoneNumber + "'";
         connection.query(query, function (err, rows) {
            
              if(rows.affectedRows == 0)
