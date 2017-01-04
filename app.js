ddbfa8df11ae75364ba67047cc5a1b46b6c67ade
arronnandotech <arronbq@gmail.com>
2017-01-04 11:07:36 -0500

    added delete route for post/get

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = 8000;
 
 var dncRouter = require('./src/routes/dncRoutes');
 app.get('/', function (req, res) {
--- src/routes/dncRoutes.js
+++ src/routes/dncRoutes.js
@@ -12,6 +12,156 @@ var viciConnection = mySql.createPool({
     });
 var apiPass = '';
 
+
+dncRouter.get('/remove', function(req,res){
+   
+    var phoneNumber = req.query.phone_number;
+    var apiKey = req.query.api_key;
+     if (apiKey != apiPass)
+    {
+        res.json({"Inserted":"false", "reason":"Invalid API Key"})
+        return;
+    }
+    if ((phoneNumber.length > 11 || phoneNumber.length < 10))
+    {
+        res.json({"Inserted":"false", "reason":"Phone Number not 10 or 11 Digits"})
+        return;
+    }
+    viciConnection.getConnection(function (err, connection){
+      if (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error connecting to database" 
+            });
+        };
+        var query = "DELETE FROM vicidial_dnc where phone_number = '" + phoneNumber + "'";
+        connection.query(query, function (err, rows) {
+           
+             if(rows.affectedRows == 0)
+            {
+                res.send(phoneNumber + "  is not currently in the DNC")
+                return;
+            }
+            if (!err) {
+                res.json({ 
+                    "Deleted": "true", "phoneNumber": phoneNumber 
+                });
+                return;
+            }           
+            else{
+                res.send(err);
+            }
+        });
+        connection.on('error', function (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error Connecting to database" 
+            });
+
+        });
+        connection.release();
+    });        
+});
+dncRouter.post('/remove', function(req,res){
+   
+    var phoneNumber = req.query.phone_number;
+    var apiKey = req.query.api_key;
+     if (apiKey != apiPass)
+    {
+        res.json({"Inserted":"false", "reason":"Invalid API Key"})
+        return;
+    }
+    if ((phoneNumber.length > 11 || phoneNumber.length < 10))
+    {
+        res.json({"Inserted":"false", "reason":"Phone Number not 10 or 11 Digits"})
+        return;
+    }
+    viciConnection.getConnection(function (err, connection){
+      if (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error connecting to database" 
+            });
+        };
+        var query = "DELETE FROM vicidial_dnc where phone_number = '" + phoneNumber + "'";
+        connection.query(query, function (err, rows) {
+           
+             if(rows.affectedRows == 0)
+            {
+                res.send(phoneNumber + "  is not currently in the DNC")
+                return;
+            }
+            if (!err) {
+                res.json({ 
+                    "Deleted": "true", "phoneNumber": phoneNumber 
+                });
+                return;
+            }           
+            else{
+                res.send(err);
+            }
+        });
+        connection.on('error', function (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error Connecting to database" 
+            });
+
+        });
+        connection.release();
+    });        
+});
+dncRouter.delete('/remove', function(req,res){
+   
+    var phoneNumber = req.query.phone_number;
+    var apiKey = req.query.api_key;
+     if (apiKey != apiPass)
+    {
+        res.json({"Inserted":"false", "reason":"Invalid API Key"})
+        return;
+    }
+    if ((phoneNumber.length > 11 || phoneNumber.length < 10))
+    {
+        res.json({"Inserted":"false", "reason":"Phone Number not 10 or 11 Digits"})
+        return;
+    }
+    viciConnection.getConnection(function (err, connection){
+      if (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error connecting to database" 
+            });
+        };
+        var query = "DELETE FROM vicidial_dnc where phone_number = '" + phoneNumber + "'";
+        connection.query(query, function (err, rows) {
+           
+             if(rows.affectedRows == 0)
+            {
+                res.send(phoneNumber + "  is not currently in the DNC")
+                return;
+            }
+            if (!err) {
+                res.json({ 
+                    "Deleted": "true", "phoneNumber": phoneNumber 
+                });
+                return;
+            }           
+            else{
+                res.send(err);
+            }
+        });
+        connection.on('error', function (err) {
+            
+            res.json({ 
+                "code": 100, "status": "Error Connecting to database" 
+            });
+
+        });
+        connection.release();
+    });        
+});
+
+
 dncRouter.get('/dnc', function(req,res){
     var phoneNumber = req.query.phone_number;
     var apiKey = req.query.api_key;
